5c356524ef6d52a187c18b5517e4f7df5cc14010
SushantVarshney21 <Sushantvars@gmail.com>
2025-01-20 16:12:26 +0530

    eeee

--- backend/socket.js
+++ backend/socket.js
@@ -7,7 +7,7 @@ let io;
 function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
-            origin: 'https://uber-clone-frontend-nine.vercel.app/',
+            origin: '*',
             methods: [ 'GET', 'POST' ]
         }
     });
