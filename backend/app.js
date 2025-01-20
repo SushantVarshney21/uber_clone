c916c09c1c63a9217af5f63281e9d8b7b7c2d440
SushantVarshney21 <Sushantvars@gmail.com>
2025-01-20 14:51:13 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,36 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const connectToDb = require('./bd/db')
-const cookieParser = require('cookie-parser')
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const connectToDb = require('./bd/db');
+const cookieParser = require('cookie-parser');
 
-const userRoutes = require("./routes/user.routes")
-const captainRoutes = require("./routes/captain.routes")
-const mapRoutes = require('./routes/map.routes')
-const rideRoutes = require('./routes/ride.routes')
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
+const mapRoutes = require('./routes/map.routes");
+const rideRoutes = require('./routes/ride.routes");
 
-connectToDb()
+connectToDb();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+// Configure CORS with allowed origin
+const corsOptions = {
+  origin: "https://vercel.com/sushant-varshneys-projects/uber-clone-frontend", // Add your frontend URL here
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+app.use(cors(corsOptions));
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use("/users",userRoutes)
-app.use("/captains",captainRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 app.use("/maps", mapRoutes);
-app.use('/rides', rideRoutes)
+app.use('/rides', rideRoutes);
 
-module.exports = app
+module.exports = app;
